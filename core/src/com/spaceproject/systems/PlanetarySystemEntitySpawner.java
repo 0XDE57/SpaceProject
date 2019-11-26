c200be4fdb79654411ebc3085582508237175938
whilow <schockwil@gmail.com>
2019-11-25 23:58:28 -0700

    Update PlanetarySystemEntitySpawner.java
    add land on another planet case
    make cleaner

--- core/src/com/spaceproject/systems/PlanetarySystemEntitySpawner.java
+++ core/src/com/spaceproject/systems/PlanetarySystemEntitySpawner.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.spaceproject.components.AIComponent;
 import com.spaceproject.components.ControlFocusComponent;
 import com.spaceproject.components.AISpawnComponent;
+import com.spaceproject.components.PlanetComponent;
 import com.spaceproject.components.TransformComponent;
 import com.spaceproject.generation.EntityFactory;
 import com.spaceproject.screens.GameScreen;
@@ -28,7 +29,7 @@ public class PlanetarySystemEntitySpawner extends IteratingSystem {
         //goal: when in a solar system (loaded planets
         //get all plaenet entitys
         //if planet has life
-    
+        
         //add to planet SpawnerComponent
         //add spawning rules
         //eg:
@@ -45,35 +46,49 @@ public class PlanetarySystemEntitySpawner extends IteratingSystem {
                 long nextInterval = MathUtils.random(spawn.min, spawn.max);
                 timer.setInterval(nextInterval, true);//also reset
                 
-                
-                if (spawn.state == AIComponent.State.attack) {
-                    //spawn ship at planets location
-                    TransformComponent transform = Mappers.transform.get(entity);
-                    Entity aiShip = EntityFactory.createAIShip(transform.pos.x, transform.pos.y, GameScreen.inSpace());
-                    AIComponent aiComponent = aiShip.getComponent(AIComponent.class);
-                    aiComponent.state = spawn.state;
-                    //find player entity, send angry ships at them
-                    ImmutableArray<Entity> players = getEngine().getEntitiesFor(Family.all(ControlFocusComponent.class).get());
-                    if (players.size() == 0) {
-                        continue;
-                    }
-                    Entity attackTarget = players.first();
-                    aiComponent.attackTarget = attackTarget;
-    
-                    getEngine().addEntity(aiShip);
-                    Gdx.app.log(this.getClass().getSimpleName(), "spawned: " + Misc.objString(entity) + " source:" + Misc.objString(entity));
-                }
-                
-                
-                //if i am another type of object, then do my own spawn rules. like maybe I am on a planet and want to spawn characters near the player
-                //TransformComponent transform = Mappers.transform.get(entity);
-                //Entity ai = EntityFactory.createCharacterAI(transform.pos.x, transform.pos.y);
-                //ai.getComponent(AIComponent.class).State = AIComponent.State.dumbwander;
-                //getEngine().addEntity(ai);
+                spawn(entity, spawn);
             }
+            
+            
+            
         }
     }
     
+    private void spawn(Entity entity, AISpawnComponent spawn) {
+        //spawn ship at planets location
+        TransformComponent transform = Mappers.transform.get(entity);
+        Entity aiShip = EntityFactory.createAIShip(transform.pos.x, transform.pos.y, GameScreen.inSpace());
+        AIComponent aiComponent = aiShip.getComponent(AIComponent.class);
+        aiComponent.state = spawn.state;
+        switch (aiComponent.state) {
+            case attack: {
+                //find player entity, send angry ships at them
+                ImmutableArray<Entity> players = getEngine().getEntitiesFor(Family.all(ControlFocusComponent.class).get());
+                if (players.size() == 0) {
+                    return;
+                }
+                Entity attackTarget = players.first();
+                aiComponent.attackTarget = attackTarget;
+            }
+            break;
+            case landOnPlanet: {
+                //todo: find a planet that is not the one we spawned on
+                //set it as target. go land.
+                ImmutableArray<Entity> planets = getEngine().getEntitiesFor(Family.all(PlanetComponent.class).get());
+                Entity destinationPlanet = planets.random();
+                if (destinationPlanet != null) {
+                    aiComponent.planetTarget = destinationPlanet;
+                }
+            }
+        }
+        
+        getEngine().addEntity(aiShip);
+        Gdx.app.log(this.getClass().getSimpleName(), "spawned: " + Misc.objString(entity) + " source:" + Misc.objString(entity));
     
-    
+        //if i am another type of object, then do my own spawn rules. like maybe I am on a planet and want to spawn characters near the player
+        //TransformComponent transform = Mappers.transform.get(entity);
+        //Entity ai = EntityFactory.createCharacterAI(transform.pos.x, transform.pos.y);
+        //ai.getComponent(AIComponent.class).State = AIComponent.State.dumbwander;
+        //getEngine().addEntity(ai);
+    }
 }
