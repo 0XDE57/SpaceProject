29425324bab577c11c90f5ae5b2e1a13d61465f3
whilow <schockwil@gmail.com>
2020-05-28 05:19:30 -0600

    Update SystemsConfig.java
    set order to priority for easier readability. added spacing by 10 between systems for room to insert systems inbetween

--- core/src/com/spaceproject/config/SystemsConfig.java
+++ core/src/com/spaceproject/config/SystemsConfig.java
@@ -14,26 +14,27 @@ public class SystemsConfig extends Config {
     public void loadDefault() {
         systems = new ArrayList<>();
         //SysCFG(entitySystem, priority, haltOnGamePause, loadInSpace, loadInWorld, loadOnDesktop, loadOnMobile)
-        systems.add(new SysCFG(AISystem.class, 3, true, true, true, true, true));
-        systems.add(new SysCFG(CameraSystem.class, 10, false, true, true, true, true));
-        systems.add(new SysCFG(CharacterControlSystem.class, 4, true, true, true, true, true));
-        systems.add(new SysCFG(ShipControlSystem.class, 4, true, true, true, true, true));
-        systems.add(new SysCFG(DebugSystem.class, 18, false, true, true, true, true));
-        systems.add(new SysCFG(DesktopInputSystem.class, 1, true, true, true, true, false));
-        systems.add(new SysCFG(ExpireSystem.class, 14, true, true, true, true, true));
-        systems.add(new SysCFG(FixedPhysicsSystem.class, 5, true, true, true, true, true));
-        systems.add(new SysCFG(HUDSystem.class, 13, false, true, true, true, true));
-        systems.add(new SysCFG(MobileInputSystem.class, 2, true, true, true, false, true));
-        systems.add(new SysCFG(OrbitSystem.class, 8, true, true, false, true, true));
+        systems.add(new SysCFG(DesktopInputSystem.class, 10, true, true, true, true, false));
+        systems.add(new SysCFG(MobileInputSystem.class, 20, true, true, true, false, true));
+        systems.add(new SysCFG(AISystem.class, 30, true, true, true, true, true));
+        systems.add(new SysCFG(CharacterControlSystem.class, 40, true, true, true, true, true));
+        systems.add(new SysCFG(ShipControlSystem.class, 50, true, true, true, true, true));
+        systems.add(new SysCFG(FixedPhysicsSystem.class, 60, true, true, true, true, true));
+        systems.add(new SysCFG(OrbitSystem.class, 70, true, true, false, true, true));
+        systems.add(new SysCFG(WorldWrapSystem.class, 80, true, false, true, true, true));
+        systems.add(new SysCFG(CameraSystem.class, 90, false, true, true, true, true));
+        systems.add(new SysCFG(SpaceRenderingSystem.class, 100, false, true, false, true, true));
+        systems.add(new SysCFG(WorldRenderingSystem.class, 110, false, false, true, true, true));
+        systems.add(new SysCFG(HUDSystem.class, 120, false, true, true, true, true));
+        systems.add(new SysCFG(ExpireSystem.class, 130, true, true, true, true, true));
+        systems.add(new SysCFG(WorldLoadingSystem.class, 140, true, false, true, true, true));
+        systems.add(new SysCFG(SpaceLoadingSystem.class, 150, false, true, false, true, true));
+        systems.add(new SysCFG(SpaceParallaxSystem.class, 160, true, true, false, true, true));
+        systems.add(new SysCFG(ScreenTransitionSystem.class, 170, true, true, true, true, true));
+        systems.add(new SysCFG(DebugSystem.class, 180, false, true, true, true, true));
         //systems.add(new SysCFG(PlanetarySystemEntitySpawner.class, 20, true, true, false, true, true));
-        systems.add(new SysCFG(RemovalSystem.class, 20, false, true, true, true, true));
-        systems.add(new SysCFG(ScreenTransitionSystem.class, 17, true, true, true, true, true));
-        systems.add(new SysCFG(SpaceLoadingSystem.class, 15, false, true, false, true, true));
-        systems.add(new SysCFG(SpaceParallaxSystem.class, 16, true, true, false, true, true));
-        systems.add(new SysCFG(SpaceRenderingSystem.class, 11, false, true, false, true, true));
-        systems.add(new SysCFG(WorldLoadingSystem.class, 15, true, false, true, true, true));
-        systems.add(new SysCFG(WorldRenderingSystem.class, 12, false, false, true, true, true));
-        systems.add(new SysCFG(WorldWrapSystem.class, 9, true, false, true, true, true));
+        systems.add(new SysCFG(RemovalSystem.class, 200, false, true, true, true, true));
+    
     }
     
     public ArrayList<SysCFG> getSystems() {
