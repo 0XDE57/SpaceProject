05f6e69079eac762856be4a737f49a36c2ed06c0
whilow <schockwil@gmail.com>
2021-01-21 20:19:27 -0700

    Update RemovalSystem.java
    respawn as player when on planet

--- core/src/com/spaceproject/systems/RemovalSystem.java
+++ core/src/com/spaceproject/systems/RemovalSystem.java
@@ -4,9 +4,13 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.Gdx;
+import com.spaceproject.SpaceProject;
 import com.spaceproject.components.ControlFocusComponent;
+import com.spaceproject.components.PlanetComponent;
 import com.spaceproject.components.RemoveComponent;
+import com.spaceproject.config.WorldConfig;
 import com.spaceproject.generation.EntityFactory;
+import com.spaceproject.screens.GameScreen;
 import com.spaceproject.utility.Mappers;
 import com.spaceproject.utility.ResourceDisposer;
 
@@ -28,8 +32,16 @@ public class RemovalSystem extends IteratingSystem {
         ControlFocusComponent controlFocusComp = Mappers.controlFocus.get(entity);
         if (controlFocusComp != null) {
             Gdx.app.log(this.getClass().getSimpleName(), "Controlled entity assumed to be player; respawning");
-            Entity newPlayer = EntityFactory.createPlayerShip(0, 0, true);
-            getEngine().addEntity(newPlayer);
+            if (GameScreen.inSpace()) {
+                Entity newPlayer = EntityFactory.createPlayerShip(0, 0, true);
+                getEngine().addEntity(newPlayer);
+            } else {
+                WorldConfig worldCFG = SpaceProject.configManager.getConfig(WorldConfig.class);
+                int mapSize = GameScreen.getCurrentPlanet().getComponent(PlanetComponent.class).mapSize;
+                int position = mapSize * worldCFG.tileSize / 2;//set  position to middle of planet
+                Entity newPlayer = EntityFactory.createPlayer(position, position);
+                getEngine().addEntity(newPlayer);
+            }
         }
     }
 }
