17dc87a5e75062695323f44d5f51da911de04054
whilow <schockwil@gmail.com>
2021-01-22 20:51:05 -0700

    Update SystemsConfig.java

--- core/src/com/spaceproject/config/SystemsConfig.java
+++ core/src/com/spaceproject/config/SystemsConfig.java
@@ -29,13 +29,13 @@ public class SystemsConfig extends Config {
         systems.add(new SysCFG(SpaceRenderingSystem.class, 100, false, true, false, true, true));
         systems.add(new SysCFG(WorldRenderingSystem.class, 110, false, false, true, true, true));
         systems.add(new SysCFG(HUDSystem.class, 120, false, true, true, true, true));
-        systems.add(new SysCFG(ExpireSystem.class, 130, true, true, true, true, true));
         systems.add(new SysCFG(WorldLoadingSystem.class, 140, true, false, true, true, true));
         systems.add(new SysCFG(SpaceLoadingSystem.class, 150, false, true, false, true, true));
         systems.add(new SysCFG(SpaceParallaxSystem.class, 160, true, true, false, true, true));
         systems.add(new SysCFG(ScreenTransitionSystem.class, 170, true, true, true, true, true));
         systems.add(new SysCFG(DebugSystem.class, 180, false, true, true, true, true));
         //systems.add(new SysCFG(PlanetarySystemEntitySpawner.class, 20, true, true, false, true, true));
+        systems.add(new SysCFG(ExpireSystem.class, 190, true, true, true, true, true));
         systems.add(new SysCFG(RemovalSystem.class, 200, false, true, true, true, true));
     
     }
