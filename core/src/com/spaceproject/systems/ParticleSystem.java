24f083933502647dd724b734e30d19aa9c76381c
whilow <schockwil@gmail.com>
2021-02-06 14:42:01 -0700

    basic fire particle effect when flying ship

--- core/src/com/spaceproject/systems/ParticleSystem.java
+++ core/src/com/spaceproject/systems/ParticleSystem.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.graphics.g2d.ParticleEmitter;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
+import com.spaceproject.components.ControllableComponent;
 import com.spaceproject.components.ParticleComponent;
 import com.spaceproject.components.TransformComponent;
 import com.spaceproject.screens.GameScreen;
@@ -20,7 +21,7 @@ import com.spaceproject.utility.Mappers;
 public class ParticleSystem extends IteratingSystem implements EntityListener {
     
     SpriteBatch spriteBatch;
-    ParticleEffect particleEffect;
+    ParticleEffect fireEffect;
     ParticleEffectPool effectPool;
     
     public ParticleSystem() {
@@ -28,12 +29,12 @@ public class ParticleSystem extends IteratingSystem implements EntityListener {
         
         spriteBatch = new SpriteBatch();
     
-        particleEffect = new ParticleEffect();
-        particleEffect.load(Gdx.files.internal("particles/test.particle"), Gdx.files.internal("particles/"));
-        particleEffect.scaleEffect(0.02f);
-        effectPool = new ParticleEffectPool(particleEffect, 20, 20);
+        fireEffect = new ParticleEffect();
+        fireEffect.load(Gdx.files.internal("particles/test.particle"), Gdx.files.internal("particles/"));
+        fireEffect.scaleEffect(0.02f);
+        effectPool = new ParticleEffectPool(fireEffect, 20, 20);
         
-        particleEffect.start();
+        fireEffect.start();
     }
     
     @Override
@@ -48,25 +49,32 @@ public class ParticleSystem extends IteratingSystem implements EntityListener {
     @Override
     protected void processEntity(Entity entity, float deltaTime) {
         ParticleComponent particle = Mappers.particle.get(entity);
-    
-        if (particle.pooledEffect != null) {
-            TransformComponent transform = Mappers.transform.get(entity);
-            Array<ParticleEmitter> emitters = particle.pooledEffect.getEmitters();
-            float engineRotation = transform.rotation * MathUtils.radDeg + 180;
-            for (int i = 0; i < emitters.size; i++) {
-                ParticleEmitter.ScaledNumericValue val = emitters.get(i).getAngle();
-                val.setHigh(engineRotation);
-                val.setLow(engineRotation);
-            }
-            particle.offset.setAngleDeg(engineRotation);
-            particle.pooledEffect.setPosition(transform.pos.x + particle.offset.x, transform.pos.y + particle.offset.y);
-            particle.pooledEffect.draw(spriteBatch, deltaTime);
-            
-            /*
-            if (particle.pooledEffect.isComplete()) {
-                particle.pooledEffect.reset();
-            }*/
+        if (particle.pooledEffect == null) {
+            return;
         }
+        
+        ControllableComponent control = Mappers.controllable.get(entity);
+        if (control != null) {
+            if (control.moveForward) {
+                particle.pooledEffect.start();
+            } else {
+                particle.pooledEffect.allowCompletion();
+            }
+        } else {
+            particle.pooledEffect.allowCompletion();
+        }
+        
+        TransformComponent transform = Mappers.transform.get(entity);
+        Array<ParticleEmitter> emitters = particle.pooledEffect.getEmitters();
+        float engineRotation = transform.rotation * MathUtils.radDeg + 180;
+        for (int i = 0; i < emitters.size; i++) {
+            ParticleEmitter.ScaledNumericValue val = emitters.get(i).getAngle();
+            val.setHigh(engineRotation);
+            val.setLow(engineRotation);
+        }
+        particle.offset.setAngleDeg(engineRotation);
+        particle.pooledEffect.setPosition(transform.pos.x + particle.offset.x, transform.pos.y + particle.offset.y);
+        particle.pooledEffect.draw(spriteBatch, deltaTime);
     }
     
     @Override
@@ -75,6 +83,7 @@ public class ParticleSystem extends IteratingSystem implements EntityListener {
         if (particle != null && particle.pooledEffect == null) {
             Gdx.app.log(this.getClass().getSimpleName(), "obtained");
             particle.pooledEffect = effectPool.obtain();
+            particle.pooledEffect.allowCompletion();//start with emitter off
         }
     }
     
