c8ca58e08e4f202778ddb6c1a0954ffbba3a3165
arbitrary hexcode <0XDE57@users.noreply.github.com>
2021-11-23 15:01:31 -0700

    disable broken animation

--- core/src/com/spaceproject/screens/animations/ForegroundAnimation.java
+++ core/src/com/spaceproject/screens/animations/ForegroundAnimation.java
@@ -3,7 +3,7 @@ package com.spaceproject.screens.animations;
 import com.badlogic.gdx.math.MathUtils;
 
 public enum ForegroundAnimation {
-    tree, delaunay, orbit, drop, crossNoise, asteroid;
+    tree, delaunay, orbit, drop, crossNoise;/*, asteroid*/;
     
     public static ForegroundAnimation random() {
         return ForegroundAnimation.values()[MathUtils.random(ForegroundAnimation.values().length - 1)];
