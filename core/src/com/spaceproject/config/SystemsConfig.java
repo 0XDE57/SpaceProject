1e2bc9e0318786006afbd7360d5eced840d47229
arbitrary hexcode <0XDE57@users.noreply.github.com>
2022-11-13 04:34:07 -0700

    render order
    fix spline render order to render on top of stars and planets

--- core/src/com/spaceproject/config/SystemsConfig.java
+++ core/src/com/spaceproject/config/SystemsConfig.java
@@ -81,14 +81,15 @@ public class SystemsConfig extends Config {
         systems.add(new SysCFG(CameraSystem.class, 100, true, true, true, true, true));
         systems.add(new SysCFG(SpaceParallaxSystem.class, 105, false, true, false, true, true));
         systems.add(new SysCFG(ParallaxRenderSystem.class, 107, false, true, true, true, true));
-        systems.add(new SysCFG(SplineRenderSystem.class, 108, false, true, true, true, true));
+        
         //systems.add(new SysCFG(SpaceDustSystem.class, 106, false, true, false, true, true));
         systems.add(new SysCFG(WorldRenderingSystem.class, 110, false, false, true, true, true));
         systems.add(new SysCFG(TileGridSystem.class, 111, false, false, true, true, true));
         systems.add(new SysCFG(Sprite2DRenderSystem.class, 120, false, true, true, true, true));
         systems.add(new SysCFG(Sprite2DShaderRenderSystem.class, 121, false, true, true, true, true));
-        systems.add(new SysCFG(AsteroidRenderSystem.class, 122, false, true, false, true, true));
-        systems.add(new SysCFG(Sprite3DRenderSystem.class, 123, false, true, true, true, true));
+        systems.add(new SysCFG(SplineRenderSystem.class, 122, false, true, true, true, true));
+        systems.add(new SysCFG(AsteroidRenderSystem.class, 123, false, true, false, true, true));
+        systems.add(new SysCFG(Sprite3DRenderSystem.class, 124, false, true, true, true, true));
         systems.add(new SysCFG(ShieldRenderSystem.class, 125, false, true, true, true, true));
         systems.add(new SysCFG(ParticleSystem.class, 130, true, true, true, true, true));
         systems.add(new SysCFG(HUDSystem.class, 200, false, true, true, true, true));
