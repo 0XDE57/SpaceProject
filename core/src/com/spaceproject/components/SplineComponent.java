fdfe557906d48b872be7246e97a58e348ec67584
arbitrary hexcode <0XDE57@users.noreply.github.com>
2022-11-14 23:37:42 -0700

    spline notes

--- core/src/com/spaceproject/components/SplineComponent.java
+++ core/src/com/spaceproject/components/SplineComponent.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.math.Vector2;
 
 public class SplineComponent implements Component {
     
+    //todo: change to vector3 to store depth index, (eg I want to render this line on top of all the others)
     public Vector2[] path;
     
     public int index;
--- core/src/com/spaceproject/systems/SplineRenderSystem.java
+++ core/src/com/spaceproject/systems/SplineRenderSystem.java
@@ -7,7 +7,9 @@ import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Disposable;
 import com.spaceproject.components.SplineComponent;
@@ -18,7 +20,8 @@ import com.spaceproject.utility.Mappers;
 public class SplineRenderSystem extends IteratingSystem implements Disposable {
         
     private final ShapeRenderer shape;
-    private int pathSize = 1000;
+    private final Color tmpColor = new Color();
+    private int maxPathSize = 1000;
     boolean debugDrawHeadTail = false;
     
     public SplineRenderSystem() {
@@ -49,15 +52,16 @@ public class SplineRenderSystem extends IteratingSystem implements Disposable {
         updateTail(spline, transform);
         
         //todo: fade nicely
-        renderTail(spline);
+        renderPath(spline);
+        //renderRainbowPath(spline);
     }
     
     //see also: https://libgdx.com/wiki/math-utils/path-interface-and-splines
     public void updateTail(SplineComponent spline, TransformComponent transform) {
         if (spline.path == null) {
             //init
-            spline.path = new Vector2[pathSize];
-            for (int v = 0; v < pathSize; v++) {
+            spline.path = new Vector2[maxPathSize];
+            for (int v = 0; v < maxPathSize; v++) {
                 spline.path[v] = new Vector2();
             }
         }
@@ -78,9 +82,7 @@ public class SplineRenderSystem extends IteratingSystem implements Disposable {
         
     }
     
-    public void renderTail(SplineComponent spline) {
-        
-        //todo: rainbow render
+    public void renderPath(SplineComponent spline) {
         for (int i = 0; i < spline.path.length-1; i++) {
             Vector2 p = spline.path[i];
             
@@ -97,10 +99,6 @@ public class SplineRenderSystem extends IteratingSystem implements Disposable {
                 } else {
                     //point to point color
                     shape.line(p.x, p.y, p2.x, p2.y, Color.BLUE, Color.GREEN);
-    
-                    //lerped point to point color
-                    //Color lerp = Color.BLUE.lerp(Color.GREEN, spline);
-                    //shape.line(p.x, p.y, p2.x, p2.y, Color.BLUE, Color.GREEN);
                 }
             } else {
                 //debug draw head to tail
@@ -109,6 +107,59 @@ public class SplineRenderSystem extends IteratingSystem implements Disposable {
                 }
             }
         }
+    }
+    
+    public void renderRainbowPath(SplineComponent spline) {
+        //todo: rainbow render
+        //xy mode
+        //change of angle: same color when going straight, 360 otherwise, doesnt care speed
+        //change of speed: 0-max, doesn't care angle
+        
+        for (int i = 0; i < spline.path.length-1; i++) {
+            Vector2 p = spline.path[i];
+            
+            //wrap tiles when position is outside of map
+            int indexWrap = i + 1 % spline.path.length;
+            if (indexWrap < 0) indexWrap += spline.path.length;
+            Vector2 p2 = spline.path[indexWrap];
+            
+            // don't draw head to tail
+            if (indexWrap != spline.index) {
+                shape.line(p.x, p.y, p2.x, p2.y, Color.WHITE, Color.GREEN);
+            } else {
+                //debug draw head to tail
+                if (debugDrawHeadTail) {
+                    shape.line(p.x, p.y, p2.x, p2.y, Color.RED, Color.WHITE);
+                }
+            }
+        }
+    }
+    
+    
+    private Color backgroundColor(OrthographicCamera cam) {
+        //still playing with these values to get the right feel/intensity of color...
+        float camZoomBlackScale = 500.0f;
+        float maxColor = 0.25f;
+        float ratio = 0.0001f;
+        float green = Math.abs(cam.position.x * ratio);
+        float blue = Math.abs(cam.position.y * ratio);
+        //green based on x position. range amount of green between 0 and maxColor
+        if ((int) (green / maxColor) % 2 == 0) {
+            green %= maxColor;
+        } else {
+            green = maxColor - green % maxColor;
+        }
+        //blue based on y position. range amount of blue between 0 and maxColor
+        if ((int) (blue / maxColor) % 2 == 0) {
+            blue %= maxColor;
+        } else {
+            blue = maxColor - blue % maxColor;
+        }
+        float red = blue + green;
+        tmpColor.set(red, green + (maxColor - red) + 0.2f, blue + (maxColor - red) + 0.1f, 1);
+        
+        tmpColor.lerp(Color.BLACK, MathUtils.clamp(cam.zoom / camZoomBlackScale, 0, 1)); //fade to black on zoom out
+        return tmpColor;
     }
     
     @Override
