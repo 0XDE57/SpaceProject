c5c089ab213f265cdc3bccde28f663dea751653b
arbitrary hexcode <0XDE57@users.noreply.github.com>
2023-01-26 17:05:17 -0700

    color for potential shader

--- core/src/com/spaceproject/components/CharacterComponent.java
+++ core/src/com/spaceproject/components/CharacterComponent.java
@@ -1,9 +1,12 @@
 package com.spaceproject.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.Color;
 
 public class CharacterComponent implements Component {
     
     public float walkSpeed;
+    
+    public Color color;
     
 }
