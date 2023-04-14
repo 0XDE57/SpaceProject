dee6dc7eb3be1528959ca8e353dea089643cd9e5
arbitrary hexcode <0XDE57@users.noreply.github.com>
2023-04-13 21:49:11 -0600

    clean up

--- core/src/com/spaceproject/systems/Box2DContactListener.java
+++ core/src/com/spaceproject/systems/Box2DContactListener.java
@@ -65,15 +65,6 @@ public class Box2DContactListener implements ContactListener {
         Object dataA = contact.getFixtureA().getBody().getUserData();
         Object dataB = contact.getFixtureB().getBody().getUserData();
         if (dataA == null || dataB == null) return;
-    
-        //todo: check collision filters here instead before grabbing entity and components?
-        //categoryBits = I am a ...
-        //maskBits     = I collide with ...
-        //contact.getFixtureA().getFilterData().categoryBits
-        //contact.getFixtureB().getFilterData().categoryBits
-        
-        //contact.getFixtureA().getUserData();
-        //contact.getFixtureB().getUserData();
 
         onCollision(contact, (Entity) dataA, (Entity) dataB);
     }
@@ -263,10 +254,6 @@ public class Box2DContactListener implements ContactListener {
     public void updateActiveContacts(World world, float deltaTime) {
         for (Contact contact : world.getContactList()) {
             if (!contact.isTouching()) continue;
-    
-            //todo: check collision filters here instead before grabbing entity and components
-            //contact.getFixtureA().getFilterData().categoryBits
-            //contact.getFixtureB().getFilterData().categoryBits
             
             Object dataA = contact.getFixtureA().getBody().getUserData();
             Object dataB = contact.getFixtureB().getBody().getUserData();
