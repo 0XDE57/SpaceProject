7ddeb75d04c57e419754bc49a4699f9e0712a248
arbitrary hexcode <0XDE57@users.noreply.github.com>
2023-04-30 13:19:19 -0600

    Update BarycenterComponent.java
    typo

--- core/src/com/spaceproject/components/BarycenterComponent.java
+++ core/src/com/spaceproject/components/BarycenterComponent.java
@@ -4,7 +4,7 @@ import com.badlogic.ashley.core.Component;
 
 /**
  * The barycenter is the center of mass of two or more bodies that orbit each other and is the point about which the bodies orbit.
- * in our use case, this will be the "celestial anchor" in where an entity holding this component will be
+ * in our use case, this will be the "celestial anchor" in where an entity holding this component will
  * act as the anchor for other entities to orbit around
  */
 public class BarycenterComponent implements Component {
