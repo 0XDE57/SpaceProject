367a8173500a065dfcd459badb17f16c93c58e55
arbitrary hexcode <0XDE57@users.noreply.github.com>
2023-05-21 22:16:42 -0600

    render space station dock

--- core/src/com/spaceproject/systems/SpaceStationSystem.java
+++ core/src/com/spaceproject/systems/SpaceStationSystem.java
@@ -1,12 +1,13 @@
 package com.spaceproject.systems;
 
 
-import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
-import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.core.Family;
-import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.CircleShape;
@@ -17,41 +18,76 @@ import com.spaceproject.components.SpaceStationComponent;
 import com.spaceproject.components.TransformComponent;
 import com.spaceproject.generation.BodyBuilder;
 import com.spaceproject.math.MyMath;
+import com.spaceproject.screens.GameScreen;
 import com.spaceproject.utility.Mappers;
 
-public class SpaceStationSystem extends EntitySystem {
-    
+public class SpaceStationSystem extends IteratingSystem {
+
+    private final ShapeRenderer shape;
     private final Vector2 tempVec = new Vector2();
-    private ImmutableArray<Entity> stations;
     
-    @Override
-    public void addedToEngine(Engine engine) {
-        stations = engine.getEntitiesFor(Family.all(SpaceStationComponent.class, TransformComponent.class).get());
+
+    public SpaceStationSystem() {
+        super(Family.all(SpaceStationComponent.class, TransformComponent.class).get());
+        shape = new ShapeRenderer();
     }
     
     @Override
     public void update(float deltaTime) {
-        for (Entity entity : stations) {
-            //keep station in orbit around parent body, don't fling out into universe...
-            SpaceStationComponent spaceStation = Mappers.spaceStation.get(entity);
-            PhysicsComponent stationPhysics = Mappers.physics.get(entity);
-            if (spaceStation.parentOrbitBody != null) {
-                TransformComponent parentTransform = Mappers.transform.get(spaceStation.parentOrbitBody);
-                //set velocity perpendicular to parent body, (simplified 2-body model)
-                float angle = MyMath.angleTo(parentTransform.pos, stationPhysics.body.getPosition()) + (spaceStation.velocity > 0 ? -MathUtils.HALF_PI : MathUtils.HALF_PI);
-                stationPhysics.body.setLinearVelocity(MyMath.vector(angle, spaceStation.velocity));
-            }
-            
-            //update docked ships
-            if (spaceStation.dockedPortA != null) {
-                updateShipInDock(spaceStation, stationPhysics, spaceStation.dockedPortA, BodyBuilder.DOCK_A_ID);
-            }
-            if (spaceStation.dockedPortB != null) {
-                updateShipInDock(spaceStation, stationPhysics, spaceStation.dockedPortB, BodyBuilder.DOCK_B_ID);
-            }
-        }
+        //enable transparency
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+
+        shape.setProjectionMatrix(GameScreen.cam.combined);
+
+        super.update(deltaTime);
+
+        Gdx.gl.glDisable(GL20.GL_BLEND);
     }
-    
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        //keep station in orbit around parent body, don't fling out into universe...
+        SpaceStationComponent spaceStation = Mappers.spaceStation.get(entity);
+        PhysicsComponent stationPhysics = Mappers.physics.get(entity);
+        if (spaceStation.parentOrbitBody != null) {
+            TransformComponent parentTransform = Mappers.transform.get(spaceStation.parentOrbitBody);
+            //set velocity perpendicular to parent body, (simplified 2-body model)
+            float angle = MyMath.angleTo(parentTransform.pos, stationPhysics.body.getPosition()) + (spaceStation.velocity > 0 ? -MathUtils.HALF_PI : MathUtils.HALF_PI);
+            stationPhysics.body.setLinearVelocity(MyMath.vector(angle, spaceStation.velocity));
+        }
+
+        //update docked ships
+        if (spaceStation.dockedPortA != null) {
+            updateShipInDock(spaceStation, stationPhysics, spaceStation.dockedPortA, BodyBuilder.DOCK_A_ID);
+        }
+        if (spaceStation.dockedPortB != null) {
+            updateShipInDock(spaceStation, stationPhysics, spaceStation.dockedPortB, BodyBuilder.DOCK_B_ID);
+        }
+
+        //draw docking pad
+        shape.begin(ShapeRenderer.ShapeType.Filled);
+        Transform transform = stationPhysics.body.getTransform();
+        for (Fixture fixture : stationPhysics.body.getFixtureList()) {
+            if (fixture.getUserData() == null) continue;
+
+            shape.setColor(Color.BLACK);
+            int dockID = (int) fixture.getUserData();
+            if (spaceStation.dockedPortA != null && dockID == BodyBuilder.DOCK_A_ID) {
+                shape.setColor(Color.WHITE);
+            }
+            if (spaceStation.dockedPortB != null && dockID == BodyBuilder.DOCK_B_ID) {
+                shape.setColor(Color.WHITE);
+            }
+
+            CircleShape dock = (CircleShape) fixture.getShape();
+            tempVec.set(dock.getPosition());
+            transform.mul(tempVec);
+            shape.circle(tempVec.x, tempVec.y, dock.getRadius());
+        }
+        shape.end();
+    }
+
     private void updateShipInDock(SpaceStationComponent spaceStation, PhysicsComponent stationPhysics, Entity dockedShip, int dockId) {
         PhysicsComponent shipPhysics = Mappers.physics.get(dockedShip);
         Transform transform = stationPhysics.body.getTransform();
@@ -83,6 +119,5 @@ public class SpaceStationSystem extends EntitySystem {
             }
         }
     }
-  
-    
+
 }
