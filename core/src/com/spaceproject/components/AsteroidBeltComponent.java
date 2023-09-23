7b1217410483f61c23b5c8f26fc56147806fd9a7
arbitrary hexcode <0XDE57@users.noreply.github.com>
2023-09-23 00:56:40 -0600

    asteroid belt composition notes
    -add color to resource type

--- core/src/com/spaceproject/components/AsteroidBeltComponent.java
+++ core/src/com/spaceproject/components/AsteroidBeltComponent.java
@@ -3,7 +3,18 @@ package com.spaceproject.components;
 import com.badlogic.ashley.core.Component;
 
 public class AsteroidBeltComponent implements Component {
-    
+
+    /* todo: belt composition. what % of asteroid types to spawn?
+    pockets?
+    distribution?
+
+    Composition[][] {
+        "Fe", 36%,
+        "Co", 14%,
+        "H2O", 2%,
+    }*/
+
+
     //distance from parent body
     public float radius;
     
--- core/src/com/spaceproject/components/ItemComponent.java
+++ core/src/com/spaceproject/components/ItemComponent.java
@@ -1,25 +1,28 @@
 package com.spaceproject.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.MathUtils;
 
 public class ItemComponent implements Component {
 
     public enum Resource {
-        NICKEL(0.1f),
-        IRON(0.2f),
-        GOLD(0.15f),
-        PLATINUM(0.05f),
-        RUBY(0.08f),
-        SAPPHIRE(0.08f),
-        EMERALD(0.07f),
-        DIAMOND(0.02f),
-        WATER(0.25f);
+        NICKEL(0.1f, new Color(192f/255f, 192f/255f, 192f/255f, 1)),
+        IRON(0.2f, new Color(128f/255f, 128f/255f, 128f/255f, 1)),
+        GOLD(0.15f, new Color(1f, 128f/255f, 128f/255f, 1)),
+        PLATINUM(0.05f, new Color(214f/255f, 214f/255f, 214f/255f, 1)),
+        RUBY(0.08f, new Color(220f/255f, 20f/255f, 60f/255f, 1)),
+        SAPPHIRE(0.08f, new Color(0f, 0f, 186f/255f, 1)),
+        EMERALD(0.07f, new Color(0, 128f/255f, 0, 1)),
+        DIAMOND(0.02f, new Color(1, 1, 1, 1)),
+        WATER(0.25f, new Color(0, 0, 1, 1));
 
         private final float dropChance;
+        private final Color color;
 
-        Resource(float dropChance) {
+        Resource(float dropChance, Color color) {
             this.dropChance = dropChance;
+            this.color = color;
         }
 
         public float getDropChance() {
