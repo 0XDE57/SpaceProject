4e5c059a654036ff8a5e579270d442f6d6ba7ef6
arbitrary hexcode <0XDE57@users.noreply.github.com>
2023-10-07 15:43:20 -0600

    star classification notes

--- core/src/com/spaceproject/components/StarComponent.java
+++ core/src/com/spaceproject/components/StarComponent.java
@@ -19,5 +19,32 @@ public class StarComponent implements Component {
     public Color colorTemp; // Black Body Radiation!
     
     //public long age; //years
+
+    enum StellarClass {
+        redDwarf,
+        yellowDwarf, //G-star
+        whiteDwarf,
+        brownDwarf,
+        blackDwarf,
+        lowMass,
+        massive,
+        redGiant,
+        redSuperGiant,
+        hyperGiant,
+        neutron,
+        pulsar
+    }
+
+    enum SpectralClass {
+        O, // ≥ 30,000 K
+        B, //10,000–30,000 K
+        A, //7,500–10,000 K
+        F, //6,000–7,500 K
+        G, //5,200–6,000 K
+        K, //3,700–5,200 K
+        M, //2,400–3,700 K
+        L,
+        T
+    }
     
 }
