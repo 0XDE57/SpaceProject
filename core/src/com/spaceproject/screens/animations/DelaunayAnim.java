448229e7b139acc6fccb631a2f6e9f4bb6b8eaec
arbitrary hexcode <0XDE57@users.noreply.github.com>
2024-10-08 18:37:59 -0600

    add triangle quality layer to animation

--- core/src/com/spaceproject/screens/animations/DelaunayAnim.java
+++ core/src/com/spaceproject/screens/animations/DelaunayAnim.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.DelaunayTriangulator;
+import com.badlogic.gdx.math.GeometryUtils;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.FloatArray;
@@ -19,7 +20,8 @@ public class DelaunayAnim extends TitleAnimation {
     FloatArray dirs;
     ShortArray triangles;
     DelaunayTriangulator delaunay = new DelaunayTriangulator();
-    
+    final Vector2 cacheVector = new Vector2();
+
     public DelaunayAnim() {
         velocity = 10;
         numPoints = 20;
@@ -42,7 +44,7 @@ public class DelaunayAnim extends TitleAnimation {
             dirs.add(dy);
         }
     }
-    
+
     @Override
     public void render(float delta, ShapeRenderer shape) {
         if (Gdx.input.justTouched()) {
@@ -69,22 +71,48 @@ public class DelaunayAnim extends TitleAnimation {
         
         shape.begin(ShapeRenderer.ShapeType.Line);
         shape.setColor(Color.BLACK);
+        //todo: remove duplicate points. spam creating points on a single point will cause crash.
+        // repro: points get stuck in corners. spam in corner
+        // Duplicate points will result in undefined behavior.
         triangles = delaunay.computeTriangles(points, false);
         for (int i = 0; i < triangles.size; i += 3) {
             //get points
             int p1 = triangles.get(i) * 2;
             int p2 = triangles.get(i + 1) * 2;
             int p3 = triangles.get(i + 2) * 2;
-            Vector2 a = new Vector2(points.get(p1), points.get(p1 + 1));
-            Vector2 b = new Vector2(points.get(p2), points.get(p2 + 1));
-            Vector2 c = new Vector2(points.get(p3), points.get(p3 + 1));
             shape.triangle(
-                    a.x, a.y,
-                    b.x, b.y,
-                    c.x, c.y);
-            //shape.circle(a.x, a.y, 2);
-            //shape.circle(b.x, b.y, 2);
-            //shape.circle(c.x, c.y, 2);
+                    points.get(p1), points.get(p1 + 1),
+                    points.get(p2), points.get(p2 + 1),
+                    points.get(p3), points.get(p3 + 1));
+        }
+        shape.end();
+
+        shape.begin(ShapeRenderer.ShapeType.Filled);
+        for (int i = 0; i < triangles.size; i += 3) {
+            //get points
+            int p1 = triangles.get(i) * 2;
+            int p2 = triangles.get(i + 1) * 2;
+            int p3 = triangles.get(i + 2) * 2;
+            float x1 = points.get(p1);
+            float x2 = points.get(p2);
+            float x3 = points.get(p3);
+            float y1 = points.get(p1 + 1);
+            float y2 = points.get(p2 + 1);
+            float y3 = points.get(p3 + 1);
+            GeometryUtils.triangleCentroid(
+                    x1, y1,
+                    x2, y2,
+                    x3, y3,
+                    cacheVector);
+            float quality = GeometryUtils.triangleQuality(
+                    cacheVector.x - x1, cacheVector.y - y1,
+                    cacheVector.x - x2, cacheVector.y - y2,
+                    cacheVector.x - x3, cacheVector.y - y3);
+            shape.setColor(0.1f, 0.1f, 0.1f, 1 - quality);
+            shape.triangle(
+                    x1, y1,
+                    x2, y2,
+                    x3, y3);
         }
         shape.end();
     }
