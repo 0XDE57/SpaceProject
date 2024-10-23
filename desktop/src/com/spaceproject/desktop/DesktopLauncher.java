2ef194bd4beaf16801f0b148b797350f54a9fab8
arbitrary hexcode <0XDE57@users.noreply.github.com>
2024-10-23 02:19:47 -0600

    enable 8x MSAA

--- desktop/src/com/spaceproject/desktop/DesktopLauncher.java
+++ desktop/src/com/spaceproject/desktop/DesktopLauncher.java
@@ -9,10 +9,7 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 
-        //todo: test MSAA
-        //config.setBackBufferConfig(8,8,8,8,16,0,8);//default
-        //config.setBackBufferConfig(0, 0, 0, 0, 0, 0, 0);
-
+        config.setBackBufferConfig(8, 8, 8, 8, 16, 0, 8);
         config.setWindowedMode(1280, 800);
         config.useVsync(true);
         config.setForegroundFPS(0);//disable limit for when vsync off
