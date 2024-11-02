3f6f39946151034d8e854a5aa3174cafe84ad8db
arbitrary hexcode <0XDE57@users.noreply.github.com>
2024-11-01 22:28:26 -0600

    slow parallax notes
    -discovered massive tile over draw issue. way too many tiles off screen.
    -eliminate new vec for cached vec

--- core/src/com/spaceproject/systems/SpaceParallaxSystem.java
+++ core/src/com/spaceproject/systems/SpaceParallaxSystem.java
@@ -1,21 +1,25 @@
 package com.spaceproject.systems;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.BufferUtils;
 import com.badlogic.gdx.utils.Disposable;
 import com.spaceproject.screens.GameScreen;
 import com.spaceproject.ui.SpaceBackgroundTile;
 import com.spaceproject.ui.SpaceBackgroundTile.TileType;
 
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 
 public class SpaceParallaxSystem extends EntitySystem implements Disposable {
-    
+
     private final SpriteBatch spriteBatch;
     private final Matrix4 projectionMatrix = new Matrix4();
     
@@ -27,18 +31,19 @@ public class SpaceParallaxSystem extends EntitySystem implements Disposable {
     private static final float star1TileDepth = 0.02f;
     private static final float star2TileDepth = 0.018f;
     private static final float star3TileDepth = 0.2f;
-    
+
+    private final Vector2 tempVec = new Vector2();
     // center tile to check for tile change
-    private Vector2 dustCenterTile;
-    private Vector2 star0CenterTile;
-    private Vector2 star1CenterTile;
-    private Vector2 star2CenterTile;
-    private Vector2 star3CenterTile;
+    private final Vector2 dustCenterTile = new Vector2();
+    private final Vector2 star0CenterTile = new Vector2();
+    private final Vector2 star1CenterTile = new Vector2();
+    private final Vector2 star2CenterTile = new Vector2();
+    private final Vector2 star3CenterTile = new Vector2();
 
     private static final int tileSize = 512;//1024;//
     private int surroundX, surroundY;// how many tiles to load around center tile
     private final ShaderProgram spaceShader;
-    
+
     
     public SpaceParallaxSystem() {
         spriteBatch = new SpriteBatch();
@@ -52,8 +57,24 @@ public class SpaceParallaxSystem extends EntitySystem implements Disposable {
         } else {
             Gdx.app.error(getClass().getSimpleName(), "shader failed to compile:\n" + spaceShader.getLog());
         }
+
+        //todo: according to async-profiler, this is one of the heavier systems. turns out we are drawing way too many tiles!
+        // SpriteBatch.flush()
+        // SpriteBatch.switchTexture()
+        //todo: another potential optimization would be to write tiles to a framebuffer or pixmap (or as many as you can stuff in a maximum texture size optimally)
+        // and draw the texture region instead. could manually keep track of which tile is in which texture and mark tiles as free
+        // when unloading. this will allow new tiles to write into the free tiles
+        int textureSize = 8192; //generally speaking: 16k = newer, 8k = most devices, 4k = older gpus (how old? dunno...)
+        //512 tiles into 8k  = 32 tiles.
+        //512 tiles into 16k = 64 tiles.
+        IntBuffer maxTextureSize = BufferUtils.newIntBuffer(1);
+        Gdx.graphics.getGL20().glGetIntegerv(GL20.GL_MAX_TEXTURE_SIZE, maxTextureSize);
+        int maxSupported = maxTextureSize.get();
+        //Gdx.app.log(getClass().getSimpleName(), "Maximum texture size: " + maxSupported);
+        //FrameBuffer frameBuffer = new FrameBuffer(Pixmap.Format.RGBA4444, maxSupported, maxSupported,false);
+        //Pixmap masterPixmap = new Pixmap(maxSupported, maxSupported, Pixmap.Format.RGBA4444);
     }
-    
+
     @Override
     public void update(float delta) {
         // load and unload tiles
@@ -77,12 +98,19 @@ public class SpaceParallaxSystem extends EntitySystem implements Disposable {
     }
     
     private void drawParallaxTiles(CameraSystem camSystem) {
+        //TextureRegion
+        //TextureRegion region = new TextureRegion()
+        //Sprite s = new Sprite()
+        //frameBuffer.
+        //masterPixmap.
+        //spriteBatch.draw();
         float centerScreenX = Gdx.graphics.getWidth() * 0.5f;
         float centerScreenY = Gdx.graphics.getHeight() * 0.5f;
         for (SpaceBackgroundTile tile : tiles) {
             float centerTile = tileSize * 0.5f;
             float drawX = ((-GameScreen.cam.position.x * tile.depth) + tile.x - centerTile) + centerScreenX;
             float drawY = ((-GameScreen.cam.position.y * tile.depth) + tile.y - centerTile) + centerScreenY;
+
             float width = tile.tex.getWidth();
             float height = tile.tex.getHeight();
             switch (tile.layerID) {
@@ -120,12 +148,40 @@ public class SpaceParallaxSystem extends EntitySystem implements Disposable {
         int prevY = surroundY;
         surroundX = (Gdx.graphics.getWidth() / tileSize) + 1;
         surroundY = (Gdx.graphics.getHeight() / tileSize) + 1;
+        //todo: too many tiles are made = overdraw offscreen
+        // must calculate surround relative to tile pos
+        // @1280x720 we only need 3-4 x and 2-3 y  ->  6 - 12 tiles max. currently there are 35 per layer for a total of 175?! bad!
+        // @1920x1080 currently there are 63 per layer for a total of 315?! terrible!
+        // @2560x1440 currently there are 91 per layer for a total of 455?! absurd!
+        // @4k must be an absolute disaster...
+        //surroundX = (int) (Gdx.graphics.getWidth() * 0.5f / tileSize);// + 1;
+        //surroundY = (int) (Gdx.graphics.getHeight() * 0.5f / tileSize);//+ 1;
+
         boolean resize = (prevX != surroundX || prevY != surroundY);
-        dustCenterTile  = updateLayer(resize, dustCenterTile, -1, dustTileDepth, SpaceBackgroundTile.TileType.Dust);
-        star0CenterTile = updateLayer(resize, star0CenterTile, 0, 0, SpaceBackgroundTile.TileType.Stars);
-        star1CenterTile = updateLayer(resize, star1CenterTile, 1, star1TileDepth, SpaceBackgroundTile.TileType.Stars);
-        star2CenterTile = updateLayer(resize, star2CenterTile, 2, star2TileDepth, SpaceBackgroundTile.TileType.Stars);
-        star3CenterTile = updateLayer(resize, star3CenterTile, 3, star3TileDepth, SpaceBackgroundTile.TileType.Stars);
+        dustCenterTile.set(updateLayer(resize, dustCenterTile, -1, dustTileDepth, SpaceBackgroundTile.TileType.Dust));
+
+/*
+        //debug
+        //this could be in tostring...
+        String out = "";
+        int count = 0;
+        for (SpaceBackgroundTile tile : tiles) {
+            String message = "(" + tile.tileX + ", " + tile.tileY + ", " + tile.layerID + ")";
+            out += message;
+            if (count >= 3) {
+                out += "\n";
+                count = 0;
+            }
+            count++;
+        }
+        Gdx.app.log(getClass().getSimpleName(), "center: " + MyMath.formatVector2(dustCenterTile, 0)
+                + " - surround:" + surroundX + "," + surroundY + ", tiles:" + tiles.size()
+                + "\n" + out);
+*/
+        star0CenterTile.set(updateLayer(resize, star0CenterTile, 0, 0, SpaceBackgroundTile.TileType.Stars));
+        star1CenterTile.set(updateLayer(resize, star1CenterTile, 1, star1TileDepth, SpaceBackgroundTile.TileType.Stars));
+        star2CenterTile.set(updateLayer(resize, star2CenterTile, 2, star2TileDepth, SpaceBackgroundTile.TileType.Stars));
+        star3CenterTile.set(updateLayer(resize, star3CenterTile, 3, star3TileDepth, SpaceBackgroundTile.TileType.Stars));
     }
     
     /**
@@ -167,7 +223,7 @@ public class SpaceParallaxSystem extends EntitySystem implements Disposable {
      * @param posY
      * @return tile that an object is in.
      */
-    private static Vector2 getTilePos(float posX, float posY, float depth) {
+    private Vector2 getTilePos(float posX, float posY, float depth) {
         float x = (posX * depth) + (tileSize * 0.5f);
         float y = (posY * depth) + (tileSize * 0.5f);
         
@@ -183,15 +239,11 @@ public class SpaceParallaxSystem extends EntitySystem implements Disposable {
         if (y < 0) {
             --tY;
         }
-        
-        return new Vector2(tX, tY);
-        //return tempVec.set(tX, tY);//todo: cache so we're not create 3 new vecs per frame
+        return tempVec.set(tX, tY);
     }
-    //static Vector2 tempVec = new Vector2();
     
     /**
      * Load tiles surrounding centerTile of specified depth.
-     *
      * @param centerTile
      * @param depth
      * @param type
@@ -213,7 +265,10 @@ public class SpaceParallaxSystem extends EntitySystem implements Disposable {
                 if (!isTileLoaded) {
                     //Gdx.app.debug(this.getClass().getSimpleName(), "Load " + type + " tile: [" + depth + "]: " + tX + ", " + tY
                     //    + " surround: " + surroundX + ", " + surroundY);
-                    tiles.add(new SpaceBackgroundTile(tX, tY, layerID, depth, tileSize, type));
+                    SpaceBackgroundTile newTile = new SpaceBackgroundTile(tX, tY, layerID, depth, tileSize, type);
+                    //todo: instead of storing individual textures
+                    //masterPixmap.drawPixmap(newTile.tex, tX * tileSize?, tY, tileSize, tileSize);
+                    tiles.add(newTile);
                 }
             }
         }
