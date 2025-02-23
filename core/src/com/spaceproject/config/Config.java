de1ca3c3e296d9e494513daaeb9c39362c1e8bfb
arbitrary hexcode <0XDE57@users.noreply.github.com>
2025-02-23 12:37:44 -0700

    minor refactor

--- core/src/com/spaceproject/config/Config.java
+++ core/src/com/spaceproject/config/Config.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.utils.SerializationException;
 
 public abstract class Config {
     
-    private final String fileName = "config/" + this.getClass().getSimpleName() + ".json";
+    private final String fileName = "config/" + getClass().getSimpleName() + ".json";
     
     public abstract void loadDefault();
     
@@ -20,36 +20,30 @@ public abstract class Config {
         Json json = new Json();
         json.setUsePrototypes(false);
         String jsonString = json.toJson(this);
-        
-        FileHandle keyFile = Gdx.files.local(fileName);
-        
+        if (prettyPrint) {
+            jsonString = json.prettyPrint(jsonString);
+        }
+
         try {
-            if (prettyPrint) {
-                keyFile.writeString(json.prettyPrint(jsonString), false);
-            } else {
-                keyFile.writeString(jsonString, false);
-            }
-            Gdx.app.log(this.getClass().getSimpleName(), "Saved: " + fileName);
-            if (prettyPrint) {
-                Gdx.app.debug(this.getClass().getSimpleName(), json.prettyPrint(jsonString));
-            } else {
-                Gdx.app.debug(this.getClass().getSimpleName(), jsonString);
-            }
+            FileHandle keyFile = Gdx.files.local(fileName);
+            keyFile.writeString(jsonString, false);
+            Gdx.app.log(getClass().getSimpleName(), "Saved: " + fileName);
+            Gdx.app.debug(getClass().getSimpleName(), jsonString);
         } catch (GdxRuntimeException ex) {
-            Gdx.app.error(this.getClass().getSimpleName(), "Could not save file: " + fileName , ex);
+            Gdx.app.error(getClass().getSimpleName(), "Could not save file: " + fileName , ex);
         }
     }
     
     public Config loadFromJson() {
         FileHandle keyFile = Gdx.files.local(fileName);
-        String objectName = this.getClass().getSimpleName();
+        String objectName = getClass().getSimpleName();
         
         try {
             if (keyFile.exists()) {
                 Json json = new Json();
                 json.setUsePrototypes(false);
                 
-                Config config = json.fromJson(this.getClass(), keyFile.readString());
+                Config config = json.fromJson(getClass(), keyFile.readString());
                 Gdx.app.log(objectName, "Loaded " + objectName + " from " + keyFile.path());
                 Gdx.app.debug(objectName, json.toJson(config));
                 return config;
